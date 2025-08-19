ce1155d1aef13866a4e85db9f7cc4f52bcc4ccce
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-08-19 09:05:27 -0300

    colocando icones, baixando biblioteca dos icones e arrumando o input e a estilização

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View, Image, TextInput } from 'react-native';
 import { style } from './styles';
 import Logo from "../../assets/logo.png";
+import {MaterialIcons} from "@expo/vector-icons";
+import { themas } from '../../global/themes';
 
 export default function Login() {
   return (
@@ -20,8 +22,23 @@ export default function Login() {
           <TextInput
             style={style.input}
           />
+          <MaterialIcons 
+          name = "email"
+          size={20}
+          color={themas.colors.gray}
+          />
         </View>
         <Text style={style.titleInput}>Senha</Text>
+        <View style={style.boxInput}>
+          <TextInput
+          style={style.input}
+          />
+          <MaterialIcons 
+          name = "remove-red-eye"
+          size={20}
+          color={themas.colors.gray}
+          />
+        </View>
         <TextInput />
       </View>
       <View style={style.boxBotton}>
