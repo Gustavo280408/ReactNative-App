3aab38fb59d1d2fb7f9cdfeba7a9c8fa1ada8618
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-08-19 07:54:37 -0300

    montando a primeira tela inicial, colocando cores e arrumando estilização da tela

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Text, View, Image, TextInput} from "react-native";
-import { style } from "./styles";
+import { Text, View, Image, TextInput } from 'react-native';
+import { style } from './styles';
 import Logo from "../../assets/logo.png";
 
 export default function Login() {
-    return (
-    < View style = {style.container} >
-        <View style={style.boxTop}>
-            <Text>Top</Text>
-            <Image
-            source={Logo}
-            />
-            <Text>Bem vindo de volta!</Text>
+  return (
+    <View style={style.container}>
+      <View style={style.boxTop}>
+        <Image
+          source={Logo}
+          style={style.logo}
+          resizeMode="contain"
+        />
+        <Text style={style.text}>Bem-Vindo de Volta</Text>
+      </View>
+      <View style={style.boxMid}>
+        <Text style={style.titleInput}>Endereço de E-mail</Text>
+        <View style={style.boxInput}>
+          <TextInput
+            style={style.input}
+          />
         </View>
-        <View style={style.boxMid}>
-            <Text>Endereço de E-mail</Text>
-            <TextInput />
-            <Text>Senha</Text>
-            <TextInput />
-        </View>
-            <View style={style.boxBotton}>
-            </View>
-        </View>
-    )
+        <Text style={style.titleInput}>Senha</Text>
+        <TextInput />
+      </View>
+      <View style={style.boxBotton}>
+      </View>
+    </View>
+  );
 }
