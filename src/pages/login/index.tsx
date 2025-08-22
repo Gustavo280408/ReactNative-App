229859f8c2ba0cb8f12a7ff765dfd5149b12b1c1
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-08-22 12:29:34 -0300

    terminando a tela inicial, colocando funções de back-end e aplicando alertas para login e senha

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,51 +1,83 @@
-import React from 'react';
-import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import { style } from './styles';
+import React, {useState } from "react";
+ 
+import { Text, View, Image, TextInput, TouchableOpacity, Alert, ActivityIndicator} from "react-native";
+ 
+import { style } from "./styles";
 import Logo from "../../assets/logo.png";
-import {MaterialIcons} from "@expo/vector-icons";
-import { themas } from '../../global/themes';
-
+import { MaterialIcons } from '@expo/vector-icons'
+import { themas } from "../../global/themes";
+ 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [passowrd ,setPassowrd] = useState('');
+  const [loading, setLoading] = useState (false);
+  async function  getLogin() {
+    try{
+      setLoading(true)
+      if(!email || !passowrd) {
+        return Alert.alert('Atenção!', 'informe os campos obrigatórios')
+      }
+      setTimeout(() => {
+        if (email == 'Matheus@gmail.com' && passowrd == '12345678'){
+          Alert.alert('Logado com sucesso!');
+        } else {
+          Alert.alert('Usuário não encontrado!');
+        }
+        setLoading(false)
+      }, 3000)
+    }catch (error) {
+      console.log(error);
+    }
+  }
+   
+  
   return (
     <View style={style.container}>
-      <View style={style.boxTop}>
-        <Image
-          source={Logo}
-          style={style.logo}
-          resizeMode="contain"
-        />
-        <Text style={style.text}>Bem-Vindo de Volta</Text>
-      </View>
-      <View style={style.boxMid}>
-        <Text style={style.titleInput}>Endereço de E-mail</Text>
-        <View style={style.boxInput}>
-          <TextInput
-            style={style.input}
-          />
-          <MaterialIcons 
-          name = "email"
-          size={20}
-          color={themas.colors.gray}
-          />
+        <View style={style.boxTop}>
+     
+      <Image
+   source={Logo}
+   style={style.logo}
+   resizeMode="contain"
+      />
+      <Text style={style.text}>Bem vindo de volta</Text>
         </View>
-        <Text style={style.titleInput}>Senha</Text>
-        <View style={style.boxInput}>
+        <View style={style.boxMid}>
+          <Text style={style.titleInput}>Endereço de e-mail</Text>
+          <View style={style.boxInput}>
           <TextInput
           style={style.input}
+          value= {email}
+          onChangeText={setEmail}
           />
-          <MaterialIcons 
-          name = "remove-red-eye"
+          <MaterialIcons
+          name = "email"
+          size={20}
+          color={themas.colors.gray}/>
+          </View>
+          <Text style={style.titleInput}>Senha</Text>
+          <View style={style.boxInput}></View>
+          <TextInput
+          style={style.input}
+          value={passowrd}
+          onChangeText={setPassowrd}
+          />
+          <MaterialIcons
+          name="remove-red-eye"
           size={20}
           color={themas.colors.gray}
-          />
+           />
         </View>
-      </View>
-      <View style={style.boxBotton}>
-        <TouchableOpacity style={style.button}>
-          <Text style={style.textButton}>Entrar</Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={style.textBotton}>Não tem conta? <Text style={{color: themas.colors.primary}}>Crie Agora!</Text></Text>
+        <View style={style.boxBotton}>
+ 
+          <TouchableOpacity style={style.button} onPress={() => getLogin()}>
+    {
+       loading ? <ActivityIndicator color={'#ffff'} size={"small"} /> : <Text style={style.textButton}>Entrar</Text>
+    }
+          </TouchableOpacity>
+         
+          </View>
+          <Text style={style.textBotton}>não tem conta? <Text style={{color: themas.colors.primary}}>crie agora!</Text></Text>
     </View>
-  )
+  );
 }
