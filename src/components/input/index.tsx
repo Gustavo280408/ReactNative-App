ddf6c7b78d63631eca6e40d06fc959d09d01ae53
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-01 14:29:06 -0300

    criando style dentro de components e comecando a componetizaçao do codigo

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,39 @@
-import React, { forwardRef } from "react";
-import { View, TextInput } from "react-native";
-import { style } from "../../pages/login/styles";
-import { MaterialIcons } from "@expo/vector-icons";
+import React, { forwardRef, Fragment} from "react";
+import { View, Text, TextInput, TextInputProps } from "react-native";
+import { style } from "../../pages/login/styles"; 
+//import { MaterialIcons}  from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import { FontAwesome, MaterialIcons, Octicons } from "@expo/vector-icons";
 
-export const Input = forwardRef(() => {
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> |
+React.ComponentType<React.ComponentProps<typeof FontAwesome>> |
+React.ComponentType<React.ComponentProps<typeof Octicons>>;
+
+type Props = TextInputProps & {
+    Iconleft?: IconComponent,
+    IconRight?: IconComponent,
+    IconLeftName?: string,
+    IconRightName?: string,
+    title?: string,
+    OnIconLeftPress?: () => void,
+    OnIconRightPress?: () => void
+}
+
+export const Input =  forwardRef (() => {
     return (
+        <>
+        <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
         <View style={style.boxInput}>
             <TextInput
                 style={style.input}
-            />
-            <MaterialIcons
+                />
+                <MaterialIcons
                 name="email"
                 size={20}
                 color={themas.colors.gray}
-            />
+                />
         </View>
+        </>
     )
+
 })
