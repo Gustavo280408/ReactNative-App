63deab00a9e200791bd0e07fc9e11553db127e6c
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-01 14:49:42 -0300

    atribuindo propriedades, componentes e biblioteca de icones, para acesso das diferentes plataformas

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, Fragment} from "react";
 import { View, Text, TextInput, TextInputProps } from "react-native";
-import { style } from "../../pages/login/styles"; 
+import { style } from "./style";
 //import { MaterialIcons}  from '@expo/vector-icons';
 import { themas } from "../../global/themes";
 import { FontAwesome, MaterialIcons, Octicons } from "@expo/vector-icons";
