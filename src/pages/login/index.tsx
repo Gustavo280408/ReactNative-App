c840a2206ebab8260a90306ddfe3e179a8d5553b
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-09 08:55:22 -0300

    atualizando o código, e agr é possivel mudar de uma tela para outra

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,8 +10,11 @@ import { MaterialIcons, Octicons } from '@expo/vector-icons'
 import { themas } from "../../global/themes"
 import { Input } from "../../components/input";
 import { Button } from "../../components/Button";
+import { useNavigation, NavigationProp } from '@react-navigation/native'
 
 export default function Login() {
+
+    const navigation = useNavigation<NavigationProp<any>>()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(true);
@@ -24,6 +27,10 @@ export default function Login() {
                 return Alert.alert('Atenção', 'Informe os campos obrigatórios!');
             }
 
+            navigation.navigate("BottomRoutes")
+
+            console.log("Logou!!!!!!!!!!!!!!!");
+
             setTimeout(() => {
                 if (email == 'gustavo@gmail.com' && password == '12345678') {
                     Alert.alert('Logado com sucesso!');
@@ -34,6 +41,8 @@ export default function Login() {
             }, 3000)
         } catch (error) {
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     }
     return (
@@ -47,60 +56,25 @@ export default function Login() {
                 <Text style={style.text}>Bem vindo de volta!</Text>
             </View>
             <View style={style.boxMid}>
-                <Input 
-                value={email}
-                onChangeText={setEmail}
-                title="ENDEREÇO E-MAIL"
-                IconRight={MaterialIcons}
-                IconRightName="email"
+                <Input
+                    value={email}
+                    onChangeText={setEmail}
+                    title="ENDEREÇO E-MAIL"
+                    IconRight={MaterialIcons}
+                    IconRightName="email"
                 />
-                <Input 
-                value={password}
-                onChangeText={setPassword}
-                title="SENHA"
-                IconRight={Octicons}
-                IconRightName={showPassword ? "eye-closed" : "eye"} //logica do olho aberto ao ver
-                secureTextEntry={showPassword}
-                onIconRightPress={() => setShowPassword(!showPassword)}
+                <Input
+                    value={password}
+                    onChangeText={setPassword}
+                    title="SENHA"
+                    IconRight={Octicons}
+                    IconRightName={showPassword ? "eye-closed" : "eye"} //logica do olho aberto ao ver
+                    secureTextEntry={showPassword}
+                    onIconRightPress={() => setShowPassword(!showPassword)}
                 />
-                {/* <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text> */}
-
-                {/* <View style={style.boxInput}>
-                    <TextInput
-                        style={style.input}
-                        value={email}
-                        onChangeText={setEmail}
-                    />
-                    <MaterialIcons
-                        name="email"
-                        size={20}
-                        color={themas.colors.gray}
-                    />
-                </View>
-                <Text style={style.titleInput}>SENHA</Text>
-                <View style={style.boxInput}>
-                    <TextInput
-                        style={style.input}
-                        value={password}
-                        onChangeText={setPassword}
-                    />
-                    <MaterialIcons
-                        name="remove-red-eye"
-                        size={20}
-                        color={themas.colors.gray}
-                    />
-                </View> */}
             </View>
             <View style={style.boxBotton}>
-                <TouchableOpacity style={style.button} onPress={() => getLogin()}>
-                    {
-                        loading ? <ActivityIndicator color={'#ffff'} size={"small"} /> :
-                            <Text style={style.textButton}>Entrar</Text>
-                    }
-                </TouchableOpacity>
-            </View>
-            <View style={style.boxBotton}>
-                <Button text="Entrar" loading={loading} onPress={() => getLogin} />
+                <Button text="Entrar" loading={loading} onPress={() => getLogin()} />
             </View>
             <Text style={style.textBotton}>Não tem conta?
                 <Text style={{ color: themas.colors.primary }}> Crie agora!</Text></Text>
