5cb5334462e9215f9439238a9b36390afdbbf6f7
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-22 14:23:43 -0300

    Estilizando os cards, colocando novas cores

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -47,5 +47,12 @@ export const style = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         gap: 10
-    }
+    },
+    titleCard: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    descriptionCard: {
+        color: themas.colors.gray
+    },
 })
