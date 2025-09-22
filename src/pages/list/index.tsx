578318069292009462d5bb5000e0ae77ca569bb3
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-22 10:28:35 -0300

    Criando Cards e componentizando eles, colocando suas propriedades, tambem realizando estilos iniciais a ele

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { FlatList } from "react-native";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
 import { style } from "./styles";
 import { Input } from "../../components/input";
 import { MaterialIcons } from '@expo/vector-icons';
@@ -27,12 +26,27 @@ const data: Array<PropCard> = [
     },
     {
         item: 2,
-        title: 'Sair pta tomar umsorvetão',
+        title: 'Sair pra tomar um sorvetão',
         description: 'página 18 ao 28',
         flag: 'urgente'
     },
 ]
 export default function List() {
+
+    const _renderCard = (item: PropCard) => {
+        return (
+                    <TouchableOpacity style={style.card}>
+            <View style={style.rowCard}>
+                {/* <Ball /> */}
+                <View>
+                    <Text>{item.title}</Text>
+                    <Text>{item.description}</Text>
+                </View>
+                {/* <Flag /> */}
+            </View>
+        </TouchableOpacity>   
+        )
+    }
     return (
         <View style={style.container}>
             <View style={style.header}>
@@ -45,12 +59,12 @@ export default function List() {
                     />
                 </View>
             </View>
-            <View>
+            <View style={style.boxList}>
                 <FlatList 
                 data={data}
                 style={{marginTop: 40, paddingHorizontal: 30}}
                 keyExtractor={(item, index) => item.item.toString()}
-                renderItem={({item, index}) => {return (<Text>{item.title}</Text>) }}
+                renderItem={({item, index}) => {return (_renderCard(item)) }}
                 />
             </View>
         </View>
--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -11,7 +11,6 @@ export const style = StyleSheet.create({
         width: '100%',
         height: Dimensions.get('window').height/6,
         backgroundColor: themas.colors.primary,
-        paddingHorizontal: 20,
         justifyContent: 'center'
     },
     greeting: {
@@ -26,5 +25,21 @@ export const style = StyleSheet.create({
         flex: 1,
         width: '100%',
         //backgroundColor: 'red'
+    },
+    card: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#FFF',
+        marginTop: 6,
+        borderRadius: 10,
+        justifyContent: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: themas.colors.lightGray
+    },
+    rowCard: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     }
 })
