edfc8ed9d23885b3509b3da48f9262abe962ea1c
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-09-24 08:25:12 -0300

    Colocando inputs no modal, estilizando eles com novos atributos dos componentes

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,7 +1,9 @@
-import React, { createContext, useContext, useRef } from "react";
+import React, { createContext, useContext, useRef, useEffect } from "react";
 import { Dimensions, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
 import { MaterialIcons, AntDesign} from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
+import { style } from "../pages/login/styles";
+import { Input } from "../components/input";
 
 export const AuthContextList: any = createContext({});
 
@@ -12,29 +14,62 @@ export const AuthProviderList = (props: any): any => {
         modalizeRef?.current?.open();
 
     }
+
+    useEffect(() => {
+        onOpen()
+    }, [])
+
     const _container = () => {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
                     <TouchableOpacity>
-                        <MaterialIcons 
-                        name="close"
-                        size={30}
+                        <MaterialIcons
+                            name="close"
+                            size={30}
                         />
                     </TouchableOpacity>
-                    <Text>Criar Tarefa</Text>
+
+                    <Text style={styles.title}>Criar Tarefa</Text>
+
                     <TouchableOpacity>
-                        <AntDesign 
-                        name="check"
-                        size={30}
+                        <AntDesign
+                            name="check"
+                            size={30}
                         />
                     </TouchableOpacity>
+
+                </View>
+                <View style={styles.content}>
+                    <Input 
+                    title="Titulo"
+                    labelStyle={styles.label}
+                    />
+                    <Input 
+                    title="Descrição"
+                    labelStyle={styles.label}
+                    height={100}
+                    multiline
+                    numberOfLines={5}
+                    />
+                </View>
+                <View style={{width: '40%'}}>
+                    <Input 
+                    title="Tempo Limite"
+                    labelStyle={styles.label}
+                    />
+                </View>
+                <View style={styles.containerFlag}>
+                    <Text style={styles.label}>Flags:</Text>
+                    <View style={{}}>
+
+                    </View>
                 </View>
             </View>
         )
     }
     return (
-        <AuthContextList.Provider value = {{ onOpen}}>
+        <AuthContextList.Provider value = {{ onOpen }}>
             {props.children}
             <Modalize
             ref={modalizeRef}
@@ -50,5 +85,32 @@ export const AuthProviderList = (props: any): any => {
 
 export const useAuth = () => useContext(AuthContextList);
 export const styles = StyleSheet.create({
-
+    container: {
+        width: '100%'
+    },
+    header: {
+        width: '100%',
+        height: 40,
+        paddingHorizontal: 40,
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    content: {
+        width: '100%',
+        paddingHorizontal: 20
+    },
+    containerFlag: {
+        width: '100%',
+        padding: 10
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#000'
+    }
 })
