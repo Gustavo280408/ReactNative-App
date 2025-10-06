134a206a413611b022d4a68d4e264f533128bf0b
GUSTAVO DANIEL RIBEIRO <gustavo.5938@aluno.pr.senac.br>
2025-10-06 12:14:17 -0300

    Renderizando tarefas, aplicando data do formato BR alem de criar novas propriedades para salvar no banco de dados

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -7,19 +7,15 @@ import { Ball } from "../../components/Ball";
 import { Flag } from "../../components/Flag";
 import { themas } from "../../global/themes";
 import { AuthContextList } from "../../context/authContext_list";
-
-type PropCard = {
-    item: number,
-    title: string,
-    description: string,
-    flag: 'urgente' | 'opcional'
-}
-
+import { AuthContextType, PropCard } from "../../global/Props";
+import { formatDateToBR } from "../../global/functions";
 
 export default function List() {
 
     const { taskList } = useContext<AuthContextType>(AuthContextList)
+
     const _renderCard = (item: PropCard) => {
+        const color = item.flag == 'Opcional' ? themas.colors.blueLight : themas.colors.red
         return (
             <TouchableOpacity style={style.card}>
                 <View style={style.rowCard}>
@@ -28,9 +24,12 @@ export default function List() {
                         <View>
                             <Text style={style.titleCard}>{item.title}</Text>
                             <Text style={style.descriptionCard}>{item.description}</Text>
+                            <Text style={style.descriptionCard}>Até {formatDateToBR(item.timeLimit)}</Text>
                         </View>
                     </View>
-                    <Flag caption="Urgente" color={themas.colors.red} />
+                    <Flag 
+                    caption={item.flag}
+                    color={color} />
                 </View>
             </TouchableOpacity>
         )
